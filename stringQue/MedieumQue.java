aaf77b591ab3826d7c8386bff215e8f2243e8209
ahilendra9singh8 <shailendra9.singh8@gmail.com>
2025-04-14 18:16:55 +0530

    second commit

--- stringQue/MedieumQue.java
+++ stringQue/MedieumQue.java
@@ -3,6 +3,27 @@ package stringQue;
 class MedieumQue {
 
     public static void main(String[] args) {
-        System.out.println("Hii");
+
+        // // 1. Reverse String
+        // String str = "Hello";
+        // String revStr = "";
+        // for (int i = str.length()-1; i >= 0; i--) {
+        // revStr += str.charAt(i);
+        // }
+        // System.out.println(revStr);
+
+        // // 2. Reverse words of string
+        // String str = "Hello How are you";
+        // String[] strArray = str.split(" ");
+        // String revStr = "";
+        // for (int i = strArray.length - 1; i >= 0; i--) {
+        //     revStr += strArray[i];
+        //     if (i > 0) {
+        //         revStr += " ";
+        //     }
+        // }
+        // System.out.println(revStr);
+
+        // 3.
     }
 }
