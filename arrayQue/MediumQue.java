cdd91cd3606242cc85afb03fc1ff7a6eaf120f35
ahilendra9singh8 <shailendra9.singh8@gmail.com>
2025-04-16 18:24:02 +0530

    ss

---
+++ arrayQue/MediumQue.java
@@ -0,0 +1,29 @@
+package arrayQue;
+
+import java.util.Arrays;
+
+public class MediumQue {
+    public static void main(String[] args) {
+        // // 1.Two sum problem
+        // int[] arr = { 2, 11, 15, 7 };
+        // int n = 9;
+        // for (int i = 0; i < arr.length; i++) {
+        // for (int j = i + 1; j <= arr.length; j++) {
+        // if ((arr[i] + arr[j]) == n) {
+        // System.out.println(i + " : " + j);
+        // break;
+        // }
+        // }
+        // }
+
+        // 2. Sort an array of 0s, 1s,2s
+        // // 2.1: Broute Force approach - using sort method
+        // int[] intArray = { 2, 0, 2, 1, 1, 0 };
+        // Arrays.sort(intArray);
+        // for (int val : intArray) {
+        // System.out.println(val);
+        // }
+
+        // 2.2: Better Approach
+    }
+}
--- stringQue/EasyQue.java
+++ stringQue/EasyQue.java
@@ -33,29 +33,118 @@ public class EasyQue {
         // String str2 = "add";
         // boolean flag = true;
         // if (str1.length() != str2.length()) {
-        //     flag = false;
+        // flag = false;
         // } else {
-        //     Map<Character, Character> map1 = new HashMap<>();
-        //     Map<Character, Character> map2 = new HashMap<>();
-        //     for (int i = 0; i < str1.length(); i++) {
-        //         char ch1 = str1.charAt(i);
-        //         char ch2 = str2.charAt(i);
+        // Map<Character, Character> map1 = new HashMap<>();
+        // Map<Character, Character> map2 = new HashMap<>();
+        // for (int i = 0; i < str1.length(); i++) {
+        // char ch1 = str1.charAt(i);
+        // char ch2 = str2.charAt(i);
 
-        //         if (map1.containsKey(ch1) && (map1.get(ch1) != ch2)) {
-        //             flag = false;
-        //         } else {
-        //             map1.put(ch1, ch2);
-        //         }
+        // if (map1.containsKey(ch1) && (map1.get(ch1) != ch2)) {
+        // flag = false;
+        // } else {
+        // map1.put(ch1, ch2);
+        // }
 
-        //         if (map2.containsKey(ch2) && (map2.get(ch2) != ch1)) {
-        //             flag = false;
-        //         } else {
-        //             map2.put(ch2, ch1);
-        //         }
-        //     }
+        // if (map2.containsKey(ch2) && (map2.get(ch2) != ch1)) {
+        // flag = false;
+        // } else {
+        // map2.put(ch2, ch1);
+        // }
+        // }
         // }
         // System.out.println(flag);
 
-        // 3.
+        // // 3.Remove Outermost Parentheses
+        // String str = "(()())(())";
+        // int cnt = 0;
+        // String result = "";
+        // for (int i = 0; i < str.length(); i++) {
+        // if (str.charAt(i) == '(') {
+        // if (cnt > 0) {
+        // result += str.charAt(i);
+        // cnt++;
+        // } else {
+        // cnt++;
+        // }
+        // } else if (str.charAt(i) == ')') {
+        // cnt--;
+        // if (cnt > 0) {
+        // result += str.charAt(i);
+        // }
+        // }
+        // }
+        // System.out.println(result);
+
+        // // 4.Largest Odd Number in String
+        // String str = "35427";
+        // String result = "";
+        // for (int i = 0; i < str.length(); i++) {
+        // String subStr = "";
+        // for (int j = i + 1; j <= str.length(); j++) {
+        // subStr = str.substring(i, j);
+        // char lastDigit = subStr.charAt(subStr.length() - 1);
+        // if ((lastDigit - '0') % 2 == 1) {
+        // if (result.equals("") || Long.parseLong(subStr) > Long.parseLong(result)) {
+        // result = subStr;
+        // }
+        // }
+        // }
+        // }
+        // System.out.println(result);
+
+        // // 5.Longest Common Prefix
+        // String[] strArray = { "flower", "flow", "flight" };
+        // String prefix = strArray[0];
+        // for (int i = 1; i < strArray.length; i++) {
+        // while (!strArray[i].startsWith(prefix)) {
+        // prefix = prefix.substring(0, prefix.length() - 1);
+        // }
+        // }
+        // System.out.println(prefix);
+
+        // // 6.Left Rotate String
+        // String str = "Hello";
+        // int d = 2;
+        // for (int i = 0; i < d; i++) {
+        // char first = str.charAt(0);
+        // str = str.substring(1) + first;
+        // }
+        // System.out.println(str);
+
+        // // 7.Right Rotate String
+        // String str = "Hello";
+        // int d = 2;
+        // for (int i = 0; i < d; i++) {
+        // char last = str.charAt(str.length() - 1);
+        // str = last + str.substring(0, str.length() - 1);
+        // }
+        // System.out.println(str);
+
+        // 8.check whether one string is a rotation of another
+        // // 8.1: 1st approach
+        // String str1 = "abcde";
+        // String str2 = "cdeab";
+        // boolean flag = false;
+        // str1 = str1 + str1;
+        // if (str1.contains(str2)) {
+        // flag = true;
+        // }
+        // System.out.println(flag);
+
+        // // 8.2:second approach
+        // String str1 = "abcde";
+        // String str2 = "cdeab";
+        // boolean flag = false;
+        // for (int i = 0; i < str1.length(); i++) {
+        //     char first = str1.charAt(0);
+        //     str1 = str1.substring(1) + first;
+        //     if (str1.equals(str2)) {
+        //         flag = true;
+        //         break;
+        //     }
+        // }
+        // System.out.println(flag);
     }
 }
