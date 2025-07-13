757534acfe9f18449ae0eacaa5e3cf6834efb2ee
Shailu08 <ssinghlodhi08@gmail.com>
2025-07-14 00:25:19 +0530

    worked on pattern.

---
+++ Testing.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Testing {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < (2 * n - 1); i++) {
+            for (int j = 0; j < (2 * n - 1); j++) {
+                int top = i;
+                int left = j;
+                int right = (2 * n - 2) - j;
+                int bottom = (2 * n - 2) - i;
+                System.out.print(n - Math.min(Math.min(top, bottom), Math.min(right, left)) + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
