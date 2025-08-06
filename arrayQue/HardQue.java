8ff0a243f2f2b72fdfd5130e4c1fc63cd4f81817
Shailu08 <ssinghlodhi08@gmail.com>
2025-08-07 01:35:39 +0530

    working on binary search of one d array

--- arrayQue/HardQue.java
+++ arrayQue/HardQue.java
@@ -637,78 +637,161 @@ public class HardQue {
         // }
         // System.out.println(cnt);
 
-        // 12.2 Optimal Approach
-        int[] arr = { 1, 3, 2, 3, 1 };
-        int n = arr.length;
-        int cnt = reverseOfPairs(arr, n);
-        System.out.println(cnt);
+        // // 12.2 Optimal Approach
+        // int[] arr = { 1, 3, 2, 3, 1 };
+        // int n = arr.length;
+        // int cnt = reverseOfPairs(arr, n);
+        // System.out.println(cnt);
+
+        // 13. Maximum Product Subarray
+        // // 13.1 ==> Better Approach
+        // int[] nums = { 1, 2, -3, 0, -4, -5 };
+        // int n = nums.length;
+        // // if(n==1) return nums[0];
+        // int max = 0;
+        // for (int i = 0; i < n; i++) {
+        // int product = 1;
+        // for (int j = i; j < n; j++) {
+        // product *= nums[j];
+        // max = Math.max(product, max);
+        // }
+        // }
+        // System.out.println(max);
+
+        // // 13.2 ==> Optimal Approch (Observations)
+        // int[] nums = { 1, 2, -3, 0, -4, -5 };
+        // int n = nums.length;
+        // int prefix = 1;
+        // int suffix = 1;
+        // int maxi = Integer.MIN_VALUE;
+        // for (int i = 0; i < n; i++) {
+        // if (prefix == 0)
+        // prefix = 1;
+        // if (suffix == 0)
+        // suffix = 1;
+        // prefix *= nums[i];
+        // suffix *= nums[n - i - 1];
+        // maxi = Math.max(maxi, Math.max(prefix, suffix));
+        // }
+        // System.out.println(maxi);
+
+        // 13.3 ==> Optimal Approch (kadane algorithm)
     }
+
+    // private static int reverseOfPairs(int[] arr, int n) {
+    // int cnt = mergeSortForReverseOfPairs(arr, 0, n - 1);
+    // return cnt;
+    // }
+
+    // private static int mergeSortForReverseOfPairs(int[] arr, int low, int high) {
+    // int cnt = 0;
+    // if (low >= high)
+    // return cnt;
+    // int mid = (low + high) / 2;
+    // cnt += mergeSortForReverseOfPairs(arr, low, mid); // left half
+    // cnt += mergeSortForReverseOfPairs(arr, mid + 1, high); // right half
+    // cnt += countPairs(arr, low, mid, high); // Modification
+    // mergeForReversePairs(arr, low, mid, high); // merging sorted halves
+    // return cnt;
+    // }
+
+    // private static int countPairs(int[] arr, int low, int mid, int high) {
+    // int right = mid + 1;
+    // int cnt = 0;
+    // for (int i = low; i <= mid; i++) {
+    // while (right <= high && arr[i] > 2 * arr[right])
+    // right++;
+    // cnt += (right - (mid + 1));
+    // }
+    // return cnt;
+    // }
+
+    // private static void mergeForReversePairs(int[] arr, int low, int mid, int
+    // high) {
+    // ArrayList<Integer> temp = new ArrayList<>(); // temporary array
+    // int left = low; // starting index of left half of arr
+    // int right = mid + 1; // starting index of right half of arr
+
+    // // storing elements in the temporary array in a sorted manner//
+
+    // while (left <= mid && right <= high) {
+    // if (arr[left] <= arr[right]) {
+    // temp.add(arr[left]);
+    // left++;
+    // } else {
+    // temp.add(arr[right]);
+    // right++;
+    // }
+    // }
+
+    // // if elements on the left half are still left //
+
+    // while (left <= mid) {
+    // temp.add(arr[left]);
+    // left++;
+    // }
+
+    // // if elements on the right half are still left //
+    // while (right <= high) {
+    // temp.add(arr[right]);
+    // right++;
+    // }
+
+    // // transfering all elements from temporary to arr //
+    // for (int i = low; i <= high; i++) {
+    // arr[i] = temp.get(i - low);
+    // }
+    // }
 
     // private static int numberOfInversions(int[] arr, int n) {
     // int cnt = mergeSort(arr, 0, n - 1);
     // return cnt;
     // }
 
-    private static int reverseOfPairs(int[] arr, int n) {
-        int cnt = mergeSort(arr, 0, n - 1);
-        return cnt;
-    }
+    // private static int mergeSort(int[] arr, int low, int high) {
+    // int cnt = 0;
+    // if (low >= high)
+    // return cnt;
+    // int mid = (low + high) / 2;
+    // cnt += mergeSort(arr, low, mid);
+    // cnt += mergeSort(arr, mid + 1, high);
+    // cnt += merge(arr, low, mid, high);
+    // return cnt;
+    // }
 
-    private static int mergeSort(int[] arr, int low, int high) {
-        int cnt = 0;
-        if (low >= high)
-            return cnt;
-        int mid = (low + high) / 2;
-        cnt += mergeSort(arr, low, mid);
-        cnt += mergeSort(arr, mid + 1, high);
-        cnt += merge(arr, low, mid, high);
-        return cnt;
-    }
+    // private static int merge(int[] arr, int low, int mid, int high) {
+    // int cnt = 0;
+    // int left = low;
+    // int right = mid + 1;
+    // ArrayList<Integer> list = new ArrayList<>();
 
-    private static int merge(int[] arr, int low, int mid, int high) {
-        int cnt = 0;
-        int left = low;
-        int right = mid + 1;
-        ArrayList<Integer> list = new ArrayList<>();
+    // while (left <= mid && right <= high) {
+    // if (arr[left] < arr[right]) {
+    // list.add(arr[left]);
+    // left++;
+    // } else {
+    // list.add(arr[right]);
+    // cnt += mid - left + 1;
+    // right++;
+    // }
+    // }
 
-        while (left <= mid && right <= high) {
-            // // for numberOfInversions
-            // if (arr[left] < arr[right]) {
-            // list.add(arr[left]);
-            // left++;
-            // } else {
-            // list.add(arr[right]);
-            // cnt += mid - left + 1;
-            // right++;
-            // }
+    // while (left <= mid) {
+    // list.add(arr[left]);
+    // left++;
+    // }
+    // while (right <= high) {
+    // list.add(arr[right]);
+    // right++;
+    // }
 
-            // for reverseOfPairs
-            if (arr[left] < 2 * arr[right]) {
-                list.add(arr[left]);
-                left++;
-            } else {
-                list.add(arr[right]);
-                cnt += mid - left + 1;
-                right++;
-            }
-        }
+    // // transfering all elements from temporary to arr //
+    // for (int i = low; i <= high; i++) {
+    // arr[i] = list.get(i - low);
+    // }
 
-        while (left <= mid) {
-            list.add(arr[left]);
-            left++;
-        }
-        while (right <= high) {
-            list.add(arr[right]);
-            right++;
-        }
-
-        // transfering all elements from temporary to arr //
-        for (int i = low; i <= high; i++) {
-            arr[i] = list.get(i - low);
-        }
-
-        return cnt;
-    }
+    // return cnt;
+    // }
 
     // private static void swap(int[] nums1, int[] nums2, int left, int right) {
     // if (nums1[left] > nums2[right]) {
---
+++ binarySearch/BS_ON_1D_Array.java
@@ -0,0 +1,224 @@
+package binarySearch;
+
+public class BS_ON_1D_Array {
+    public static void main(String[] args) {
+        // // 1.Binary Search to find X in sorted array
+        // // 1.1 => Iterative Implementation
+        // int[] nums = { -1, 0, 3, 5, 9, 12 };
+        // int target = 9;
+        // int low = 0;
+        // int high = nums.length - 1;
+        // int index = -1;
+        // while (low <= high) {
+        // int mid = (low + high) / 2;
+        // if (target == nums[mid]) {
+        // index = mid;
+        // break;
+        // } else if (target > nums[mid]) {
+        // low = mid + 1;
+        // } else {
+        // high = mid - 1;
+        // }
+        // }
+        // System.out.println(index);
+
+        // // 1.2 => Recursive implementation:
+        // int[] nums = { 3, 4, 6, 7, 9, 12, 16, 17 };
+        // int target = 6;
+        // int low = 0;
+        // int high = nums.length - 1;
+        // int ind = binarySearch(nums, low, high, target);
+        // System.out.println(ind);
+
+        // 2.Implement Lower Bound
+        // // 2.1=> Broute Force Approach (Using linear search)
+        // int[] nums = { 3, 5, 8, 15, 19 };
+        // int n= nums.length;
+        // int x = 9;
+        // int result = n;
+        // for (int i = 0; i < nums.length; i++) {
+        // if (nums[i] >= x) {
+        // result = i;
+        // break;
+        // }
+        // }
+        // System.out.println(result);
+
+        // // 2.2 => Optimal Approach (Using Binary Search):
+        // int[] nums = { 3, 5, 8, 15, 19 };
+        // int x = 9;
+
+        // int low = 0;
+        // int high = nums.length - 1;
+        // int lowerBound = nums.length;
+        // while (low <= high) {
+        // int mid = (low + high) / 2;
+        // if (nums[mid] >= x) {
+        // lowerBound = mid;
+        // high = mid - 1;
+        // } else if (nums[mid] < x) {
+        // low = mid + 1;
+        // }
+        // }
+        // System.out.println(lowerBound);
+
+        // 3.Implement Upper Bound
+        // // 3.1 => Broute Force Approach (Using linear search):
+        // int[] nums = { 3, 5, 8, 9, 15, 19 };
+        // int x = 9;
+        // int n = nums.length;
+        // int result = n;
+        // for (int i = 0; i < n; i++) {
+        // if (nums[i] > x) {
+        // result = i;
+        // break;
+        // }
+        // }
+        // System.out.println(result);
+
+        // // 3.2 => Optimal Approach (Using Binary Search):
+        // int[] nums = { 3, 5, 8, 9, 15, 19 };
+        // int x = 9;
+        // int n = nums.length;
+        // int result = n;
+        // int low = 0;
+        // int high = n - 1;
+        // while (low <= high) {
+        // int mid = (low + high) / 2;
+        // if (nums[mid] > x) {
+        // result = mid;
+        // high = mid - 1;
+        // } else {
+        // low = mid + 1;
+        // }
+        // }
+        // System.out.println(result);
+
+        // // 4.Search Insert Position(same lower bound)
+        // int[] nums = {1,2,4,7};
+        // int x=6;
+        // int low = 0;
+        // int high = nums.length - 1;
+        // int lowerBound = nums.length;
+        // while (low <= high) {
+        // int mid = (low + high) / 2;
+        // if (nums[mid] >= x) {
+        // lowerBound = mid;
+        // high = mid - 1;
+        // } else if (nums[mid] < x) {
+        // low = mid + 1;
+        // }
+        // }
+        // System.out.println(lowerBound);
+
+        // // 5.Floor/Ceil in Sorted Array
+        // int[] nums = {3, 4, 4, 7, 8, 10};
+        // int x = 5;
+        // // int[] nums = { 3, 4, 4, 7, 8, 10 };
+        // // int x = 8;
+        // int floor = nums[0];
+        // int ceil = nums[nums.length - 1];
+        // int low = 0;
+        // int high = nums.length - 1;
+        // while (low <= high) {
+        // int mid = (low + high) / 2;
+        // if (nums[mid] <= x) {
+        // floor = nums[mid];
+        // low = mid + 1;
+        // }
+        // if (nums[mid] >= x) {
+        // ceil = nums[mid];
+        // high = mid - 1;
+        // }
+        // }
+        // System.out.println(floor);
+        // System.out.println(ceil);
+
+        // // 6.Find the first or last occurrence of a given number in a sorted array
+        // // 6.1 ==> my approach
+        // int[] nums = { 3, 4, 13, 13, 13, 20, 40 };
+        // int target = 13;
+        // int index = -1;
+        // int low = 0;
+        // int high = nums.length - 1;
+        // while (low <= high) {
+        // int mid = (low + high) / 2;
+        // if (nums[mid] == target) {
+        // index = mid;
+        // // first occurrence
+        // high = mid - 1;
+        // // last occurrence
+        // // low = mid + 1;
+
+        // } else if (nums[mid] < target) {
+        // low = mid + 1;
+        // } else {
+        // high = mid - 1;
+        // }
+        // }
+        // System.out.println(index);
+
+        // // 6.2 ==> using lowebound or upperbound
+        // int[] nums = { 3, 4, 13, 13, 13, 20, 40 };
+        // int target = 13;
+        // int first = lb(nums, target);
+        // int second = ub(nums, target);
+        // if (nums[first] != target || first == nums.length) {
+        //     first = -1;
+        //     second = -1;
+        // } else {
+        //     second = second - 1;
+        // }
+        // System.out.println(first);
+        // System.out.println(second);
+
+        // 7.Count occurrences of a number in a sorted array with duplicates
+
+    }
+
+    private static int lb(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int index = nums.length;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] >= target) {
+                index = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    private static int ub(int[] nums, int target) {
+        int index = nums.length;
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (nums[mid] > target) {
+                index = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return index;
+    }
+
+    // public static int binarySearch(int[] nums, int low, int high, int target) {
+    // if (low > high)
+    // return -1; // Base case.
+
+    // // Perform the steps:
+    // int mid = (low + high) / 2;
+    // if (nums[mid] == target)
+    // return mid;
+    // else if (target > nums[mid])
+    // return binarySearch(nums, mid + 1, high, target);
+    // return binarySearch(nums, low, mid - 1, target);
+    // }
+
+}
